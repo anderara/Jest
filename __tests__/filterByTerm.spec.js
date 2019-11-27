52981d0089be2f931371db72bf3033b95759b2b6
anderara <andc91@gmail.com>
2019-11-27 17:19:48 +0100

    Edit function to make it case insesitive

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -9,5 +9,6 @@ describe("Filter function", () => {
         ]
         const output = [{ id: 3, url: "https://www.link3.dev" }]
         expect(filterByTerm(input, "link")).toEqual(output)
+        expect(filterByTerm(input, "LINK")).toEqual(output)
     })
 })
